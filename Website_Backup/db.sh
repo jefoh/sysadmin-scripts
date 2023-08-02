f417ead08efbf2f3b8c4f7703eb416b9abc3bb7a
jefoh <46411467+jefoh@users.noreply.github.com>
2023-08-02 12:38:39 -0500

    Added db.sh for editing database creds

---
+++ Website_Backup/db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# These are settings for you to customize the script to fit your needs
+# Include the trailing slash while setting any path variables
+
+# Local database credentials
+DBNAME=''
+DBUSER=''
+DBPASS=''
